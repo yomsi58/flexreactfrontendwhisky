6afb6b76d03c68584e722f350dd6f26f19db8baa
Raymond <feuzeu58@gmail.com>
2023-12-12 12:28:43 -0500

    updates

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,12 +5,7 @@ export const About = () => {
     return (
         <div>
             <Navbar />
-            Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
+           
         </div>
     )
 }
